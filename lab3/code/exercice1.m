f3032fed51770be5adb3e6e2ffc097e9168586bd
augustin72 <viot.augustin@gmail.com>
2017-11-09 21:04:27 +0100

    created first files

---
+++ lab3/code/exercice1.m
@@ -0,0 +1,9 @@
+%This file contains the script for to launch to test the function
+
+clear;  %clean memomry
+clc;    %clean screen
+
+
+%load a file :
+dataset1 = load('xxx');
+
---
+++ lab3/code/knnClassifier.m
@@ -0,0 +1,9 @@
+%this file contains the knn classifier function
+
+
+function [targetTest,errorRate] = knnClassifier(trainingDataset, testDataset)
+%here we have the descritpion of the function
+
+
+%Here the code
+
